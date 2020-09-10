aa62dee7d5cbee130bc807f84d82199a3fc53a60
Robin Malfait <malfait.robin@gmail.com>
2020-09-10 12:28:05 +0200

    prefer built-in type for arguments/parameters of a function

--- src/utils/disposables.ts
+++ src/utils/disposables.ts
@@ -1,21 +1,19 @@
-type ArgumentsOf<F extends Function> = F extends (...args: infer A) => any ? A : never
-
 export function disposables() {
   const disposables: Function[] = []
 
   const api = {
-    requestAnimationFrame(...args: ArgumentsOf<typeof requestAnimationFrame>) {
+    requestAnimationFrame(...args: Parameters<typeof requestAnimationFrame>) {
       const raf = requestAnimationFrame(...args)
       api.add(() => cancelAnimationFrame(raf))
     },
 
-    nextFrame(...args: ArgumentsOf<typeof requestAnimationFrame>) {
+    nextFrame(...args: Parameters<typeof requestAnimationFrame>) {
       api.requestAnimationFrame(() => {
         api.requestAnimationFrame(...args)
       })
     },
 
-    setTimeout(...args: ArgumentsOf<typeof setTimeout>) {
+    setTimeout(...args: Parameters<typeof setTimeout>) {
       const timer = setTimeout(...args)
       api.add(() => clearTimeout(timer))
     },
